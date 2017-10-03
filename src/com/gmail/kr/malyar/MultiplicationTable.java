0ef332fb1d6d37be1fe2190cca1da3b85c7b0eb8
malyarDS <malyar.kr@gmail.com>
2017-10-03 10:21:39 +0300

    first commit

---
+++ src/com/gmail/kr/malyar/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package com.gmail.kr.malyar;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		
+		for(int b = 1;b <= 10;b++){
+			int c = a * b;
+			System.out.println(a+" X " + b + " = "+c);
+		}
+		
+	}
+
+}
